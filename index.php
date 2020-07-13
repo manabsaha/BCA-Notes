47036fc448776bd78652ffc527ad5f93b7655be4
Manab <46393531+manabsaha@users.noreply.github.com>
2020-07-13 23:20:08 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BCA</title>
+	<link rel="stylesheet" href="static/style.css">
+</head>
+<body>
+	<!-- Navbar -->
+	<nav>
+		<img src="static/img/jb.jpg" alt="JBC">
+		<label for="college_department">
+			<span id="college">JAGANNATH BAROOAH COLLEGE</span><br>
+			<span id="department">DEPARTMENT OF COMPUTER SCIENCE</span>
+		</label>
+		<ul>
+			<li class="active">HOME</li>
+			<li>FACULTY</li>
+			<li>RESULTS</li>
+			<li>LOGIN</li>
+		</ul>
+	</nav>
+	<!-- Background Div -->
+	<div id="background-div">
+		<div id="downloads">
+			<div>
+				<p>Looking for&nbsp&nbsp<span>NOTES</span>&nbsp&nbspor<br>
+				<span>QUESTION PAPERS?</span></p><br>
+				<button>OPEN DOWNLOADS</button>
+			</div>
+		</div>
+		<div id="bottom-nav">
+			<ul>
+				<li>PHOTO GALLERY</li>
+				<li>FEEDBACK</li>
+				<li>ADMISSIONS</li>
+				<li>FACILITIES</li>
+				<li>CONTACT US</li>
+			</ul>
+		</div>
+	</div>
+	<!-- About Div -->
+	<div id="about-div">
+		<div id="image">
+			<img src="static/img/about.png" alt="About">
+		</div>
+		<div id="about">
+			<div>
+				<p id="p1">ABOUT THE DEPARTMENT</p>
+				<p id="p2">The Department of Computer Science of J.B. College was formally inaugurated by the Hon'ble Director of Higher Education, Dr H.K. Sahoo, on 19th January 2004.</p>
+				<p id="p3">The department consists of the following courses:</p>
+				<p id="p4">• Three year (six semesters) Bachelors in Computer Applications (BCA).</p>
+				<p id="p5">• One year (two semesters) Post Graduate Diploma in Computer Application (PGDCA).</p>
+			</div>
+		</div>
+	</div>
+	<!-- Footer -->
+	<footer>
+		<span>&copy All Rights Reserved 2020</span>
+	</footer>
+</body>
+</html>
