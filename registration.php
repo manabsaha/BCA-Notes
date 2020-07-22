8c253022621b20bd51497276947e4f05c17ceff3
Manab <46393531+manabsaha@users.noreply.github.com>
2020-07-22 11:05:21 +0530

    register UI

--- registration.php
+++ registration.php
@@ -30,18 +30,111 @@
 <head>
 	<title>BCA</title>
 </head>
+<style>
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: sans-serif;
+	}
+	body{
+		display: flex;
+		justify-content: center;
+		height: 100vh;
+		align-items: center;
+	}
+	#reg-bg{
+		background-image: linear-gradient(240deg, #9fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
+		position: fixed;
+		height: 100vh;
+		width: 100%;
+	}
+	#reg-overlay{
+		box-shadow:inset 0 0 0 2000px rgba(0, 0, 0, 0.3);
+		height: 60%;
+		width: 60%;
+		position: fixed;
+		top:50%;
+		left:50%;
+		transform: translate(-50%,-50%);
+		justify-content: center;
+	}
+	#register-form{
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		width: 30%;
+		float:left;
+		background-color: #fff;
+	}
+	#register-form form{
+		width: 80%;
+		margin: 10% 10%;
+	}
+	#register-form h3{
+		text-align: center;
+		font-size: 1.5em;
+		margin-bottom: 2%;
+	}
+	#register-form input,#register-form select{
+		width: 100%;
+		padding: 10px;
+		margin: 6px 0;
+		font-size: 1em;
+	}
+	#register-form button{
+		width: 100%;
+		padding: 10px;
+		margin: 6px 0;
+		border: none;
+		border-radius: 4px;
+		background-color: #1C5E91;
+		color: #fff;
+		font-size: 1.4em
+	}
+	#reg-logo{
+		position: relative;
+		width: 15%;
+		margin: 0 0 0 2%
+	}
+	#reg-logo img{
+		width: 100%;
+		height: 100%;
+	}
+	#reg-home-btn{
+		position: fixed;
+		bottom: 2%;
+		left: 48%;
+		font-size: 1.2em;
+		border: none;
+		border-radius: 4px;
+		background-color: #EE3737;
+		color: #fff;
+		padding: 1%;
+	}
+</style>
 <body>
-<form action="registration.php" method="POST">
-	<input type="text" placeholder="name" name="name">
-	<input type="text" placeholder="email" name="email">
-	<select name="batch">
-		<option value="2017-20">2017-20</option>
-		<option value="2018-21">2018-21</option>
-		<option value="2019-22">2019-22</option>
-	</select>
-	<input type="password" placeholder="new password">
-	<input type="password" placeholder="confirm password" name="password">
-	<button>SUBMIT</button>
-</form>
+	<div id="reg-bg"></div>
+	<div id="reg-overlay"></div>
+	<div id="register-form">
+		<form action="registration.php" method="POST">
+			<h3>REGISTER</h3>
+			<input type="text" placeholder="Name" name="name" required="true">
+			<input type="text" placeholder="Email" name="email" required="true">
+			<select name="batch" required="true">
+				<option value="" disabled="true" selected="true">BATCH</option>
+				<option value="2017-20">2017-20</option>
+				<option value="2018-21">2018-21</option>
+				<option value="2019-22">2019-22</option>
+			</select>
+			<input type="password" placeholder="New password" required="true">
+			<input type="password" placeholder="Confirm password" name="password" required="true">
+			<button onclick="return confirm('Details can\'t be changed once submitted. Sure to submit?')">SUBMIT</button>
+		</form>
+	</div>
+	<div id="reg-logo">
+		<img src="static/img/jblogo.png" alt="">
+	</div>
+	<button id="reg-home-btn" onclick="window.location.href='/bca'">Back to home</button>
 </body>
 </html>
