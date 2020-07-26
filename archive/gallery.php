eacb9d85ef96ffb5df82570863bd2207f17eb7d0
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2020-07-26 15:33:35 +0530

    gallery done

--- archive/gallery.php
+++ archive/gallery.php
@@ -1,3 +1,27 @@
+<?php
+	$folder_arr=array();
+	//MYSQL connection
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "BCA";
+	$conn = mysqli_connect($servername, $username, $password,$dbname);
+	if(!$conn){
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	else{
+		$sql = "SELECT DISTINCT folder from photo_gallery";
+		$tmp = mysqli_query($conn, $sql);
+		foreach ($tmp as $key => $value) {
+			$t=explode("/", $value["folder"]);
+			array_push($folder_arr,(end($t)));
+		}
+	}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,8 +46,33 @@
 		</ul>
 	</nav>
 	<!-- Background Div -->
-	
 	<div id="gallery-div" style="position: relative;">
+		<?php foreach ($folder_arr as $key => $folder): ?> 
+			<div class="folder">
+				<h3 style=""><?php echo $folder;?></h3>
+				<div class="thumb">
+					<?php 
+					$sql = "select name, disp_name from photo_gallery where folder = 'photos/".$folder."'";
+					$url_arr = mysqli_query($conn, $sql);
+					echo mysqli_error($conn);
+
+
+					//seperates image name from URL
+					$img_arr=array();
+					foreach ($url_arr as $key => $url) {
+						$t = explode("/", $url["name"]);
+						array_push($img_arr, end($t));
+					}
+
+					//loops through the image names
+					foreach ($img_arr as $key => $img): ?>
+						<img src="<?php echo "../upload/thumbnails/".$img;?>">
+					<?php endforeach;?>
+				</div>
+			</div>
+		<?php endforeach;?>
+	</div>
+	<!--div id="gallery-div" style="position: relative;">
 		<div class="folder">
 			<h3 style="">Folder Name</h3>
 			<div class="thumb">
@@ -91,7 +140,7 @@
 				<img src="../static/img/thumb.jpg">
 			</div>
 		</div>
-	</div>
+	</div-->
 	<!-- Footer -->
 	<footer>
 		<span>&copy All Rights Reserved 2020</span>
